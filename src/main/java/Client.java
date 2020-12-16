535e955890e7bcbd32a47dc2922ab17887a12569
Revelation <revnyirongo@live.com>
2020-12-17 00:13:44 +0200

    comments on the client class

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+
         Scanner scanner=new Scanner(System.in);
         System.out.println("Please enter Regimen:");
-        String RegimenInput=scanner.next();
+        String RegimenInput=scanner.next(); //Receiving input for Regimen
+
         FactoryRegimen factoryRegimen= new FactoryRegimen();
         Regimen first=factoryRegimen.GetDrugs(RegimenInput);
         System.out.println("Enter Weight to determine dosage and date of next appointment: \n");
         int WeightInput=scanner.nextInt();
-        first.validate(RegimenInput,WeightInput);
+        first.validate(RegimenInput,WeightInput);//passing weight
 
     }
 }
